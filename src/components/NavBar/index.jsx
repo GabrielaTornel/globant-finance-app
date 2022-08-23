50b3afbe7a61a5390fabdf868bc940a1a4906d89
gabriela tornel <tornelgabriela@gmail.com>
2022-08-23 12:57:23 -0500

    componente Navbar

--- src/components/NavBar/index.jsx
+++ src/components/NavBar/index.jsx
@@ -1,7 +1,13 @@
-import React from 'react'
-
-export default function index() {
+import React from "react";
+import { Nav } from "rsuite";
+import CloseIcon from '@rsuite/icons/Close';
+import './index.css'
+export const Navbar = () => {
   return (
-    <div>index</div>
-  )
-}
+    <Nav className="containerNav">
+     
+      <Nav.Item>email@gmail.com</Nav.Item>
+      <Nav.Item icon={<CloseIcon />}></Nav.Item>
+    </Nav>
+  );
+};
