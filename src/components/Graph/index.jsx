fe45fe68692fba839c27edb2a2686da45a0a17c2
Luz Angela Martinez <luzangela.lic@gmail.com>
2022-08-25 15:50:26 -0500

    ()[Graph]: Categoories and colors Graphic

--- src/components/Graph/index.jsx
+++ src/components/Graph/index.jsx
@@ -1,39 +1,64 @@
-
-// export const categories = ["Restaurante", "Salud", "Regalos", "Entretenimiento", "Servicios", "Familia", "Comestibles", "Ocio" ]
 import React, { useEffect, useState } from "react";
-// import { getInfo } from "../../Helpers/crud";
-import { Chart as ChartJS, ArcElement, Tooltip, Legend } from 'chart.js';
-import { Doughnut } from 'react-chartjs-2';
+import { Chart as ChartJS, ArcElement, Tooltip, Legend } from "chart.js";
+import { Doughnut } from "react-chartjs-2";
+import { getTotalSumFromCategories } from "../../Helpers/expenceOperations";
 
 ChartJS.register(ArcElement, Tooltip, Legend);
-export const data = {
-  labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
-  datasets: [
-    {
-      label: '# of Votes',
-      data: [12, 19, 3, 5, 2, 3],
-      backgroundColor: [
-        'rgba(255, 99, 132, 0.2)',
-        'rgba(54, 162, 235, 0.2)',
-        'rgba(255, 206, 86, 0.2)',
-        'rgba(75, 192, 192, 0.2)',
-        'rgba(153, 102, 255, 0.2)',
-        'rgba(255, 159, 64, 0.2)',
-      ],
-      borderColor: [
-        'rgba(255, 99, 132, 1)',
-        'rgba(54, 162, 235, 1)',
-        'rgba(255, 206, 86, 1)',
-        'rgba(75, 192, 192, 1)',
-        'rgba(153, 102, 255, 1)',
-        'rgba(255, 159, 64, 1)',
-      ],
-      borderWidth: 1,
-    },
+
+const graphStyles = {
+  backgroundColor: [
+    "rgba(242, 191, 39, 0.5)",
+    "rgba(199, 22, 19, 0.5)",
+    "rgba(164, 193, 54, 0.5)",
+    "rgba(255, 189, 109, 0.5)",
+    "rgba(165, 227, 212, 0.5)",
+    "rgba(109, 186, 255, 0.5)",
+    "rgba(174, 116, 81, 0.5)",
+    "rgba(255, 202, 137, 0.5)",
   ],
+  borderColor: [
+    "rgba(242, 191, 39, 1)",
+    "rgba(199, 22, 19, 1)",
+    "rgba(164, 193, 54, 1)",
+    "rgba(255, 189, 109, 1)",
+    "rgba(165, 227, 212, 1)",
+    "rgba(109, 186, 255, 1)",
+    "rgba(174, 116, 81, 1)",
+    "rgba(255, 202, 137, 1)",
+  ],
+  borderWidth: 1,
 };
 
 export const Graph = () => {
+  const [graphConfig, setGraphConfig] = useState({});
+  const [isConfigReady, setIsConfigReady] = useState(false);
 
-  return <Doughnut data={data} />;
-}
+  useEffect(() => {
+    const setup = async () => {
+      const totalExpensesData = await getTotalSumFromCategories();
+      const labels = [];
+      const data = [];
+      Object.entries(totalExpensesData).forEach(([index, expense]) => {
+        const { totalSum, category } = expense;
+        labels.push(category);
+        data.push(totalSum);
+      });
+      console.log(labels, data);
+      setGraphConfig({
+        labels,
+        datasets: [
+          {
+            label: "Categorias",
+            data,
+            ...graphStyles
+          },
+        ],
+      });
+      setIsConfigReady(true);
+    };
+    setup();
+  }, []);
+  return <>{isConfigReady && <Doughnut data={graphConfig} />}</>;
+};
+
+export default Graph;
