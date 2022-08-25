9c30cf9cbf6810ff5cd28d00e48ba992f851f8c3
Marisol Daza <marisol.dazaa@gmail.com>
2022-08-25 13:00:09 -0500

    (chore)[dashboard]: filter

--- src/pages/Dashboard/Dashboard.jsx
+++ src/pages/Dashboard/Dashboard.jsx
@@ -1,42 +1,30 @@
-import React from "react";
 import "rsuite/styles/index.less";
-
-import { Navbar } from "../../components/NavBar";
-import { Filter } from "../../components/Filter/Filter";
-
-
 import "./index.css";
 import { ListItems } from "../../components/List-items";
 import { Graph } from "../../components/Graph";
-
+import { Filter } from "../../components/Filter/Filter";
 
 function Dashboard() {
-  console.log(localStorage)
+  console.log(localStorage);
   return (
-    
     <>
-
-      <header>
-        {/* <Navbar /> */}
-      </header>
+      <header>{/* <Navbar /> */}</header>
       <main className="dashboard-container">
         <section className="containerGraph">
           <Graph />
-          <Filter/>
+          <Filter />
         </section>
 
         <section>
           <h1>Dashboard</h1>
         </section>
-        <section > 
+        <section>
           <ListItems />
         </section>
-      
       </main>
       <> </>
     </>
   );
-
 }
 
 export default Dashboard;
